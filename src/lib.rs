2a9336cca8e378856f2ba59198344b5def8a80f8
Kamen Yovchevski <57301959+KYovchevski@users.noreply.github.com>
2022-06-07 15:00:26 +0200

    📜 Add comments explaining RgbaSurface's usage. (#5)
    * Add comments explaining RgbaSurface's usage
    
    * Correct poor wording in comment
    
    * Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,11 +16,20 @@ pub mod bc6h;
 pub mod bc7;
 pub mod etc1;
 
+/// Describes a 2D image to block-compress.
 #[derive(Debug, Copy, Clone)]
 pub struct RgbaSurface<'a> {
+    /// The pixel data for the image.
+    /// The data does not need to be tightly packed, but if it isn't, stride must be different from `width * 4`.
+    ///
+    /// Expected to be at least `stride * height`.
     pub data: &'a [u8],
+    /// The width of the image in texels.
     pub width: u32,
+    /// The height of the image in texels.
     pub height: u32,
+    /// The stride between the rows of the image, in bytes.
+    /// If `data` is tightly packed, this is expected to be `width * 4`.
     pub stride: u32,
 }
 
