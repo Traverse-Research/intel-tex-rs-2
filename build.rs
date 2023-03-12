647dc7fdd49114eee7c98a53e05cb3283a4d4c05
Marijn Suijten <marijn@traverseresearch.nl>
2023-03-12 18:11:31 +0100

    Add `aarch64` GNU and Android prebuilts (#17)
    * Add `aarch64` GNU and Android prebuilts
    
    * Import new and rebuilt prebuilts from CI

--- build.rs
+++ build.rs
@@ -8,41 +8,44 @@ ISPC project file builds the kernels as such:
 fn compile_kernel() {
     use ispc_compile::{bindgen::builder, Config, TargetISA};
 
-    Config::new()
-        .file("vendor/ispc_texcomp/kernel.ispc")
-        .opt_level(2)
-        .woff()
-        .target_isas(vec![
+    let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_isas = match target_arch.as_str() {
+        "x86" | "x86_64" => vec![
             TargetISA::SSE2i32x4,
             TargetISA::SSE4i32x4,
             TargetISA::AVX1i32x8,
             TargetISA::AVX2i32x8,
             TargetISA::AVX512KNLi32x16,
             TargetISA::AVX512SKXi32x16,
-        ])
-        .bindgen_builder(builder().allowlist_function(r#"CompressBlocks(BC\dH?|ETC1)_ispc"#))
+        ],
+        "arm" | "aarch64" => vec![
+            // TargetISA::Neoni32x4,
+            TargetISA::Neoni32x8,
+        ],
+        x => panic!("Unsupported target architecture {}", x),
+    };
+
+    Config::new()
+        .opt_level(2)
+        .woff()
+        .target_isas(target_isas.clone())
         .out_dir("src/ispc")
+        .file("vendor/ispc_texcomp/kernel.ispc")
+        .bindgen_builder(builder().allowlist_function(r#"CompressBlocks(BC\dH?|ETC1)_ispc"#))
         .compile("kernel");
 
     Config::new()
-        .file("vendor/ispc_texcomp/kernel_astc.ispc")
         .opt_level(2)
         .woff()
-        .target_isas(vec![
-            TargetISA::SSE2i32x4,
-            TargetISA::SSE4i32x4,
-            TargetISA::AVX1i32x8,
-            TargetISA::AVX2i32x8,
-            TargetISA::AVX512KNLi32x16,
-            TargetISA::AVX512SKXi32x16,
-        ])
+        .target_isas(target_isas)
+        .out_dir("src/ispc")
+        .file("vendor/ispc_texcomp/kernel_astc.ispc")
         .bindgen_builder(
             builder()
                 .allowlist_function("astc_rank_ispc")
                 .allowlist_function("astc_encode_ispc")
                 .allowlist_function("get_programCount"),
         )
-        .out_dir("src/ispc")
         .compile("kernel_astc");
 
     // ASTC encoder `extern "C"`'s some code, so we need to make sure to link
