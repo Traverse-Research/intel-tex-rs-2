c14d927539a9a19ea8f9e974886bdefa3ef82dae
Marijn Suijten <marijn@traverseresearch.nl>
2025-07-02 11:14:57 +0200

    Add `ispc_texcomp_astc` compiled against static Windows/MSVC CRT
    When compiling upstream's `ispc_texcomp_astc.cpp`, `cc-rs` defaults
    to using the dynamic MSVC CRT, and this is what the pregenerated
    and checked-in `.lib` file uses/requires as well.  This conflicts
    with our internal project that sets `target-feature=+crt-static`
    (which subsequently only applies to everything that's compiled on
    the spot), and appears to be an error of the form `rust-lld: error:
    /failifmismatch: mismatch detected for 'RuntimeLibrary'` since Rust
    1.86.
    
    To solve this, append the CRT version to the binary name based on
    `+crt-static` in `CARGO_CFG_TARGET_FEATURE` when compiling and when
    linking the prebuilt, and check the new libraries into the repository.
    
    New binaries are from https://github.com/Traverse-Research/intel-tex-rs-2/actions/runs/16024542690, for all platforms and architectures.

--- build.rs
+++ build.rs
@@ -4,11 +4,21 @@ ISPC project file builds the kernels as such:
 <Outputs Condition="'$(Configuration)|$(Platform)'=='Release|x64'">$(TargetDir)%(Filename).obj;$(TargetDir)%(Filename)_sse2.obj;$(TargetDir)%(Filename)_sse4.obj;$(TargetDir)%(Filename)_avx.obj;$(TargetDir)%(Filename)_avx2.obj;</Outputs>
 */
 
+fn windows_mt_suffix() -> &'static str {
+    let target_features = std::env::var("CARGO_CFG_TARGET_FEATURE").unwrap();
+    if target_features.contains("crt-static") {
+        "-MT"
+    } else {
+        "-MD"
+    }
+}
+
 #[cfg(feature = "ispc")]
-fn compile_kernel() {
+fn main() {
     use ispc_compile::{bindgen::builder, Config, TargetISA};
 
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    #[allow(deprecated, reason = "Pending ISPC update")]
     let target_isas = match target_arch.as_str() {
         "x86" | "x86_64" => vec![
             TargetISA::SSE2i32x4,
@@ -22,7 +32,7 @@ fn compile_kernel() {
             // TargetISA::Neoni32x4,
             TargetISA::Neoni32x8,
         ],
-        x => panic!("Unsupported target architecture {}", x),
+        x => panic!("Unsupported target architecture {x}"),
     };
 
     Config::new()
@@ -54,20 +64,30 @@ fn compile_kernel() {
     // is unable to deadstrip it and the requirement for a single symbol.
     let out_dir = std::env::var("OUT_DIR").unwrap();
 
+    let target_family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+    let target_mt = if target_family == "windows" {
+        windows_mt_suffix()
+    } else {
+        ""
+    };
+
     cc::Build::new()
         .include(out_dir)
         .file("vendor/ispc_texcomp/ispc_texcomp_astc.cpp")
         .out_dir("src/ispc")
         // Append the target triple since we'll be checking this file in, just like
         // the compiled kernels above.
+        // On Windows, also append whether we were compiled against the dynamic or static
+        // multithread library, so that we can copy and link against this correctly when using
+        // pregenerated libraries.
         .compile(&format!(
-            "ispc_texcomp_astc{}",
+            "ispc_texcomp_astc{}{target_mt}",
             std::env::var("TARGET").unwrap()
         ));
 }
 
 #[cfg(not(feature = "ispc"))]
-fn compile_kernel() {
+fn main() {
     use std::path::Path;
 
     ispc_rt::PackagedModule::new("kernel")
@@ -80,19 +100,20 @@ fn compile_kernel() {
 
     // Manually link ispc_texcomp_astc since it's not an ISPC module but C++.
 
-    let libname = format!("ispc_texcomp_astc{}", std::env::var("TARGET").unwrap());
-    let libpath = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/ispc");
+    let mut libname = format!("ispc_texcomp_astc{}", std::env::var("TARGET").unwrap());
     let libfile = match std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap().as_str() {
-        "unix" => format!("lib{}.a", libname),
-        "windows" => format!("{}.lib", libname),
+        "unix" => format!("lib{libname}.a"),
+        "windows" => {
+            let windows_mt = windows_mt_suffix();
+            libname += windows_mt;
+            format!("{libname}.lib")
+        }
         x => panic!("Unknown target family {}", x),
     };
 
     println!("cargo:rustc-link-lib=static={}", libname);
-    println!("cargo:rerun-if-changed={}", libpath.join(libfile).display());
-    println!("cargo:rustc-link-search=native={}", libpath.display());
-}
 
-fn main() {
-    compile_kernel();
+    let libpath = Path::new(env!("CARGO_MANIFEST_DIR")).join("src/ispc");
+    println!("cargo:rustc-link-search=native={}", libpath.display());
+    println!("cargo:rerun-if-changed={}", libpath.join(libfile).display());
 }
